9de6c625273aff992926e657b41b5f4fa3f8e999
Valeria Parise <parisevaleria@gmail.com>
2024-01-09 12:06:04 +0100

    es

--- EserciziSB/Cp2_PrimaWebAppv2/es1_ControllerGetPost/src/main/java/com/example/es1_ControllerGetPost/controllers/NameController.java
+++ EserciziSB/Cp2_PrimaWebAppv2/es1_ControllerGetPost/src/main/java/com/example/es1_ControllerGetPost/controllers/NameController.java
@@ -5,18 +5,22 @@ import org.springframework.web.bind.annotation.*;
 import com.example.es1_ControllerGetPost.service.NameService;
 
 @RestController
+@RequestMapping("/")
 public class NameController {
     @Autowired
     private NameService nameService;
-    @GetMapping(path = "/{name}")
-    public String getName(@RequestParam(value = "name" , required = true) String name){
+    @GetMapping(path = "")
+    public String getName(@RequestParam(required = true) String name)
+    {
         return nameService.printName(name);
     }
 
-    @PostMapping(path = "/{name}")
-    public String getRevName (@RequestParam(value = "name" , required = true)String name){
+    @PostMapping(path = "")
+    public String getRevName (
+            @RequestParam String name){
        return nameService.revName(name);
     }
 
+  //  http://localhost:8080/?name=Maria
 
 }
